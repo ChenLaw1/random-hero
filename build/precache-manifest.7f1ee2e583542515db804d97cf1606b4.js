f2ddda0945ab8e5a16d20806ac3e8e87fea198b0
Talyn1 <James.BradfordHang@bestbuy.com>
2018-12-04 16:54:23 -0600

    Add /build folder

---
+++ build/precache-manifest.7f1ee2e583542515db804d97cf1606b4.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eaf720584d9ea3f6409a2d8005665c1",
+    "url": "/random-hero/static/media/mercy.4eaf7205.png"
+  },
+  {
+    "revision": "6259a5f868b18e580644",
+    "url": "/random-hero/static/css/main.b52bfc3d.chunk.css"
+  },
+  {
+    "revision": "0ba5523b5b952a7a8595",
+    "url": "/random-hero/static/js/1.0ba5523b.chunk.js"
+  },
+  {
+    "revision": "1bce95c7160b767a71bc",
+    "url": "/random-hero/static/js/runtime~main.1bce95c7.js"
+  },
+  {
+    "revision": "881a372b2558ba24563785ec2b13710c",
+    "url": "/random-hero/static/media/ana.881a372b.png"
+  },
+  {
+    "revision": "e52c07ea78353fdd7697d4b171c01682",
+    "url": "/random-hero/static/media/ashe.e52c07ea.png"
+  },
+  {
+    "revision": "2ec38da3451c2f228745205c007c1a51",
+    "url": "/random-hero/static/media/bastion.2ec38da3.png"
+  },
+  {
+    "revision": "bc6824f157acf768cda9305ddd64a855",
+    "url": "/random-hero/static/media/brigitte.bc6824f1.png"
+  },
+  {
+    "revision": "63e7ecdd259755a5088e4b1fd4ef9f76",
+    "url": "/random-hero/static/media/doomfist.63e7ecdd.png"
+  },
+  {
+    "revision": "aa40d97bda7b48fcffe38356ad0baf78",
+    "url": "/random-hero/static/media/dva.aa40d97b.png"
+  },
+  {
+    "revision": "edf5cb613d6bd0ea8501248430f25071",
+    "url": "/random-hero/static/media/genji.edf5cb61.png"
+  },
+  {
+    "revision": "54115af1cd0446f95a936fc57ae318e8",
+    "url": "/random-hero/static/media/hanzo.54115af1.png"
+  },
+  {
+    "revision": "f7c64cd52d9d7571a58683ae2b9718b7",
+    "url": "/random-hero/static/media/junkrat.f7c64cd5.png"
+  },
+  {
+    "revision": "93401b88ee0e6e930b02c82346f3ddc8",
+    "url": "/random-hero/static/media/lucio.93401b88.png"
+  },
+  {
+    "revision": "f4332d7fd86738917b0095c8dfa233f4",
+    "url": "/random-hero/static/media/mccree.f4332d7f.png"
+  },
+  {
+    "revision": "13e7cb0a75a0f350ef684fda6663f064",
+    "url": "/random-hero/static/media/mei.13e7cb0a.png"
+  },
+  {
+    "revision": "6259a5f868b18e580644",
+    "url": "/random-hero/static/js/main.6259a5f8.chunk.js"
+  },
+  {
+    "revision": "5d3bb0659b732c4468b29d694d399dcb",
+    "url": "/random-hero/static/media/moira.5d3bb065.png"
+  },
+  {
+    "revision": "2af55a0deb334a7a9afb6025b783f2ec",
+    "url": "/random-hero/static/media/orisa.2af55a0d.png"
+  },
+  {
+    "revision": "22847a6ee4e9da67fb15c9de58f1ed23",
+    "url": "/random-hero/static/media/pharah.22847a6e.png"
+  },
+  {
+    "revision": "88940f66f257407125b5d4e1eb54f153",
+    "url": "/random-hero/static/media/reaper.88940f66.png"
+  },
+  {
+    "revision": "b9639ca0efdfac7a0fcf869d0787c3b8",
+    "url": "/random-hero/static/media/roadhog.b9639ca0.png"
+  },
+  {
+    "revision": "d331b6a3c8fd279b5eb760b43a49ba5d",
+    "url": "/random-hero/static/media/soldier76.d331b6a3.png"
+  },
+  {
+    "revision": "7ff0ee4baaed9dab5184f517870c80f7",
+    "url": "/random-hero/static/media/sombra.7ff0ee4b.png"
+  },
+  {
+    "revision": "41619d339fd92d0431bcc2276ce1afcf",
+    "url": "/random-hero/static/media/symmetra.41619d33.png"
+  },
+  {
+    "revision": "20f6b8af39fb4c5dd987c177072d957c",
+    "url": "/random-hero/static/media/torbjorn.20f6b8af.png"
+  },
+  {
+    "revision": "67d3f08e150de79952c44bc99f9553ef",
+    "url": "/random-hero/static/media/tracer.67d3f08e.png"
+  },
+  {
+    "revision": "9b5dee800ad246c83a9ea541fe4eed37",
+    "url": "/random-hero/static/media/widowmaker.9b5dee80.png"
+  },
+  {
+    "revision": "0814092ec60ea7cddcc3b91b987114e6",
+    "url": "/random-hero/static/media/winston.0814092e.png"
+  },
+  {
+    "revision": "549ee471105755aad117444017e855af",
+    "url": "/random-hero/static/media/wreckingball.549ee471.png"
+  },
+  {
+    "revision": "0a3f77746a473f9e0f11344460004148",
+    "url": "/random-hero/static/media/zenyatta.0a3f7774.png"
+  },
+  {
+    "revision": "9d40172e3b292870dc99e03830c06daf",
+    "url": "/random-hero/index.html"
+  }
+];
